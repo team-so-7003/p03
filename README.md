883963dc51437ca57e8eac5a3f2ddcc1790cca27
NonSpinning <hugo.ibarra@ciencias.unam.mx>
2022-09-24 17:44:31 -0500

    BFS

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # practica02
+Cambios: 
+- Implementación BFS
+- Ejemplar y ejecución en main
+- Cola.c copiada a Cola.h para importar
+- PNG visualizando el ejemplar y su matriz de adyacencias.
--- main.c
+++ main.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "pila.h"
-#include "cola.c"
+//#include "pila.h"
+#include "cola.h"
 
 int balanced_parenthesis(char* expr);
-void bfs(int** graph);
+void bfs(int** digraph, int vertices);
 
 int main()
-{   
-    //PRUEBA DE FUNCIONAMIENTO DE COLA
-    int elem1 = 1;
-    int elem2 = 2;
-    int elem3 = 3;
-    void* p1 = &elem1;
-    void* p2 = &elem2;
-    void* p3 = &elem3;
-
-    Cola* cola = crea_cola(crea_nodo_simple(p1));
-    queue(cola, p2);
-    queue(cola, p3);
-    
-    while (!cola_es_vacia(cola)){
-        printf("%d\n",*(int*)dequeue(cola));
-    }
-    //FIN DE PRUEBA DE FUNCIONAMIENTO DE COLA
-    
-    
+{
     // Creación de casos de ejemplo
+    int **matrix;
+    int row = 7, col = 7;
+    matrix = (int**)malloc(row*sizeof(int*));
+    for (int i=0; i < row; i++)
+    matrix[i]= (int*)malloc(col*sizeof(int));
+    matrix[0][1] = 1;
+    matrix[0][3] = 1;
+    matrix[1][2] = 1;
+    matrix[1][4] = 1;
+    matrix[2][5] = 1;
+    matrix[3][2] = 1;
+    matrix[4][3] = 1;
+    matrix[5][6] = 1;
+    matrix[6][3] = 1;
     // Prueba con dichos casos
+    bfs(matrix, 7);
     return 0;
 }
 
@@ -37,7 +34,7 @@ int main()
  * @param expr Una expresión de paréntesis a realizar
  * @return 0 si no hay balance, 1 e.o.c.
  */
-int balanced_parernthesis(char expr)
+int balanced_parenthesis(char* expr)
 {
     // TODO: Impl
     return -1;
@@ -46,10 +43,50 @@ int balanced_parernthesis(char expr)
 /**
  * Realiza un recorrido BFS sobre una gráfica
  * Demostrar el uso de una cola.
- * @param graph - Una gráfica representada en una matriz de adyacencia
+ * @param digraph - Una gráfica dirigida representada en una matriz de adyacencia, una gráfica simple puede verse
+ * como un caso particular en el que graph[i][j] = graph[j][i] para toda i,j.
+ * @param vertices - El número de vértices de la gráfica, notesé que graph
+ * tiene que ser de tamaño vertices * vertices.
  */
-void bfs(int** graph)
-{
-    // TODO: Impl
+void bfs(int** digraph, int vertices)
+{   
+    printf("Recorrido BFS: ");
+    //Create visited array
+    int visited[vertices];
+    for(int i = 0; i < vertices; i++)
+    visited[i] = 0;
+    //Create queque
+    //Put first node in queue
+    void* headElemptr = malloc(sizeof(int));
+    *((int*)headElemptr) = 0;
+    Nodo_Ligado* head = crea_nodo_simple(headElemptr);
+    Cola* Q = crea_cola(head);
+    
+    void* currentIndex = 0;
+    int current = -1;
+    void* toAddptr;
+    //Loop until queue's empty:
+    while (!cola_es_vacia(Q))
+    {
+        //Dequeue node
+        currentIndex = dequeue(Q);
+        current = *((int*)currentIndex);
+        //If visited, throw away.
+        if (!visited[current]){
+        //If unvisited:
+            //Mark as visited
+            visited[current] = 1;
+            //Put all neighbors in queue
+            for (int j = 0; j < vertices; j++){
+                if (digraph[current][j]){
+                    toAddptr =  malloc(sizeof(int));
+                    *((int*)toAddptr) = j;
+                    queue(Q, toAddptr);
+                }
+            }
+            //Print that it has been visited  
+            printf("%d ", current);
+        }
+    }        
+    printf("\n");
 }
-
